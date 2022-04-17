8ff95c4916070eb64a8a3145ec32c0924d2729e0
MomoDaBro <Momofqais@gmail.com>
2022-04-16 22:12:03 -0500

    Added jump feature but need to fix double jumping issue

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -6,12 +7,28 @@ public class PlayerMovement : MonoBehaviour{
 
     public float forwardForce = 2000;
     public float sidewaysForce = 500;
-    
+
+    private bool canJump = false;
+    private bool shouldJump = false;
+
+    private void OnCollisionStay(Collision collisionInfo){
+        if (collisionInfo.gameObject.layer == 8){
+            canJump = true;
+            
+        }
+    }
+
+    private void Update(){
+        if (Input.GetKeyDown(KeyCode.Space) && canJump){
+            shouldJump = true;
+            canJump = false;
+        }
+    }
 
     // Update is called once per frame
     void FixedUpdate(){
         rb.AddForce(0, 0, forwardForce * Time.deltaTime);
-        
+
         if (Input.GetKey("d")){
             rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
@@ -19,10 +36,17 @@ public class PlayerMovement : MonoBehaviour{
         if (Input.GetKey("a")){
             rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
-        
-        if (Input.GetKey("s")){
-            rb.AddForce(0 , 0, -forwardForce * Time.deltaTime);
+
+        if (Input.GetKeyDown("s")){
+            rb.AddForce(0, 0, -forwardForce * Time.deltaTime);
         }
+
+        if (shouldJump){
+            rb.AddForce(0, 1000, 0);
+            shouldJump = false;
+            canJump = false;
+        }
+
         
     }
 }
